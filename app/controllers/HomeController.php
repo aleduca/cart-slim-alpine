09da2ea0c3b7fa2247ddb5ed72f24f3a71d597b8
Alexandre Eduardo Cardoso <aleducardoso@gmail.com>
2022-06-10 16:26:46 -0300

    Query modified

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -10,8 +10,17 @@ class HomeController
     public function index(Request $request, Response $response)
     {
         $query = new Query;
-        $products = $query->select('products', 'id,name,price')->get();
-        // var_dump($products);
+        // select * from products p inner join categories c on p.id = c.product_id inner join categoryName cn on c.category_id = cn.id
+        $products = $query->select('products p', 'p.id,cn.name as category, p.name as name, price')
+        ->where('p.id', '>', 2)
+        ->join('categories as c', 'p.id = c.product_id')
+        ->join('categoryName cn', 'c.category_id = cn.id')
+        ->group('id,category,name,price')
+        ->get();
+
+
+        $products2 = $query->select('products p')->get();
+
         view('home', ['title' => 'Home','products' => $products]);
 
         return $response;
--- app/database/Query.php
+++ app/database/Query.php
@@ -6,21 +6,52 @@ class Query
     private ?string $sql;
     private array $where = [];
     private array $binds = [];
+    private array $joins = [];
     private ?string $limit;
     private ?string $order;
+    private ?string $group;
+
+    private function resetQuery()
+    {
+        if (!empty($this->sql)) {
+            $this->sql = null;
+        }
+        if (!empty($this->where)) {
+            $this->where = [];
+        }
+        if (!empty($this->binds)) {
+            $this->binds = [];
+        }
+        if (!empty($this->joins)) {
+            $this->joins = [];
+        }
+        if (!empty($this->limit)) {
+            $this->limit = null;
+        }
+        if (!empty($this->order)) {
+            $this->order = null;
+        }
+        if (!empty($this->group)) {
+            $this->group = null;
+        }
+    }
 
     public function select(string $from, string $fields = '*')
     {
+        $this->resetQuery();
         $this->sql = "select {$fields} from {$from}";
         return $this;
     }
 
-    public function where(string $field, string $operator, string $value, string $logic = null)
+    public function where(string $field, string $operator, string $value, ?string $logic = null)
     {
-        $query = "{$field} {$operator} :{$field}";
+        if (str_contains($field, '.')) {
+            $placeholder = str_replace('.', '', $field);
+        }
+        $query = "{$field} {$operator} :{$placeholder}";
         $query.= !empty($logic) ? ' '.$logic : '';
         $this->where[] = trim($query);
-        $this->binds[$field] = $value;
+        $this->binds[$placeholder] = $value;
 
         return $this;
     }
@@ -37,12 +68,28 @@ class Query
         return $this;
     }
 
-    public function dump()
+    public function join(string $table, string $relationship, string $type = 'inner')
     {
+        // inner join categories c on p.id = c.product_id inner join categoryName cn on c.category_id = cn.id
+        $this->joins[] = " {$type} join {$table} on {$relationship}";
+        return $this;
+    }
+
+    public function group(string $group)
+    {
+        $this->group = " group by $group";
+        return $this;
+    }
+
+    private function dump()
+    {
+        $this->sql.= (!empty($this->joins)) ? implode(' ', $this->joins): '';
         $this->sql.= (!empty($this->where)) ? ' where '.implode(' ', $this->where): '';
         $this->sql.= $this->order ?? '';
         $this->sql.= $this->limit ?? '';
+        $this->sql.= $this->group ?? '';
         var_dump($this->sql);
+        // die();
     }
 
     public function get()
